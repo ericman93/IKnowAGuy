edcf8d906b5422219af8f69248137865dec12e03
Gur Ronen <Gur Ronen>
2015-11-19 22:55:41 +0200

    evaritung

---
+++ modules/service/ratingDirvective.js
@@ -0,0 +1,12 @@
+/**
+ * Created by GurRo on 11/19/2015.
+ */
+
+angular.module('iKnowAGuyApp.service')
+    .directive('rating',
+        function () {
+            return {
+                template: 'modules/service/rating.html'
+            }
+
+        });
